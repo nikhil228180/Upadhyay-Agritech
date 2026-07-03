681afd2ca15c75d153ed980bddecc957183618bf
Nikhil <nikhil228180@gmail.com>
2026-07-03 18:52:08 +0530

    feat: initialize backend server and implement modular API services for auth, contact, and property modules

--- client/src/api/propertyApi.js
+++ client/src/api/propertyApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://upadhyay-agritech-rvz5.vercel.app/api",
 });
 
 // Attach token if present
